c2ad4cf9f32faa238d375ec16c7e8a84377b1f69
cuiyuesheng <syouhotcys@yeah.net>
2025-10-13 17:00:10 +0800

    1

--- src/components/PlayerRepeatToggle.tsx
+++ src/components/PlayerRepeatToggle.tsx
@@ -2,7 +2,6 @@
 import { colors } from "@/constants/theme";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { ComponentProps } from "react";
-import { ViewProps } from "react-native";
 
 import { match } from "ts-pattern";
 
@@ -11,17 +10,22 @@ type IconName = ComponentProps<typeof MaterialCommunityIcons>["name"]
 
 const repeatOrder = [
     "repeat-off",
-    "repeat-one",
+    "repeat-once",
     "repeat"
 ] as const
 export default function PlayerRepeatToggle({ ...iconProps }: IconProps) {
-    const repeatMode = false
+    const repeatMode = useTrackPlayerRepeatMode()
     const icon = match(repeatMode)
         .returnType<IconName>()
-        .with(false, () => "repeat-off")
-        .with(true, () => "repeat-one")
+        .with(0, () => "repeat-off")
+        .with(1, () => "repeat-once")
+        .with(2, () => "repeat")
         .otherwise(() => "repeat-off")
 
-    return <MaterialCommunityIcons name={icon} onPress={;
-        toggleRepeatMode} color={ colors.icon } {...iconProps } />
+    return <MaterialCommunityIcons
+        name={icon}
+        onPress={toggleRepeatMode}
+        color={colors.icon}
+        {...iconProps}
+    />
 }
